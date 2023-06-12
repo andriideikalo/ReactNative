e4fbeee902daa7877b571630c328e171b4171a32
Dagaz <wlada801@gmail.com>
2023-06-12 18:41:11 +0300

    fixHomeScreen

--- Screens/HomeScreen.jsx
+++ Screens/HomeScreen.jsx
@@ -1,9 +1,18 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
+
 export const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.titleContainer}>Публікації</Text>
+      <View style={styles.containerTitle}>
+        <Text style={styles.title}>Публікації</Text>
+      </View>
+      <View style={styles.containerImage}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/log-out.png")}
+        />
+      </View>
     </View>
   );
 };
@@ -11,23 +20,37 @@ export const HomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "#FFFFFF",
-    height: 44,
+    height: 88,
     position: "absolute",
     left: 0,
     right: 0,
-    top: 55,
   },
-  titleContainer: {
+  containerTitle: {
+    flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    marginTop: 44,
+  },
+  title: {
     color: "#212121",
-    alignItems: "center",
     fontSize: 17,
     fontWeight: "500",
-    lineHeight: 35,
+    lineHeight: 22,
+    paddingVertical: 11,
+    paddingHorizontal: 49,
+  },
+  containerImage: {
+    alignItems: "flex-end",
+    marginRight: 16,
+    marginTop: 44,
+  },
+  image: {
+    width: 24,
+    height: 24,
   },
 });
 
