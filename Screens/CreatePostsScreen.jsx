efac0ed1e048c37264afb1435d460caecb2e61a6
Dagaz <wlada801@gmail.com>
2023-06-21 17:33:07 +0300

    fix

--- Screens/CreatePostsScreen.jsx
+++ Screens/CreatePostsScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   StyleSheet,
@@ -8,6 +8,8 @@ import {
   TextInput,
 } from "react-native";
 import * as ImagePicker from "expo-image-picker";
+import { Camera } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
 
 export const CreatePostsScreen = ({ navigation }) => {
   const [isFocusedName, setIsFocusedName] = useState(false);
@@ -16,6 +18,28 @@ export const CreatePostsScreen = ({ navigation }) => {
   const [locality, setLocality] = useState("");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [photoCard, setPhotoCard] = useState(null);
+  // підключення камери
+  const [hasPermission, setHasPermission] = useState(null);
+  const [cameraRef, setCameraRef] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await ImagePicker.requestCameraPermissionsAsync();
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  // __________________________
 
   const handleFocusName = () => {
     setIsFocusedName(true);
@@ -79,23 +103,67 @@ export const CreatePostsScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       {/* <View style={styles.photoContainer}> */}
-      <TouchableOpacity
+      {/* <TouchableOpacity
         style={styles.photoContainer}
-        onPress={handleSelectPhoto}>
-        <Image
+        onPress={handleSelectPhoto}> */}
+      <Camera style={styles.camera} type={type} ref={setCameraRef}>
+        <View style={styles.photoView}>
+          <TouchableOpacity
+            style={styles.flipContainer}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}>
+            <Text style={{ fontSize: 18, marginBottom: 10, color: "white" }}>
+              {" "}
+              Flip{" "}
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={async () => {
+              if (cameraRef) {
+                const { uri } = await cameraRef.takePictureAsync();
+                await MediaLibrary.createAssetAsync(uri);
+              }
+            }}>
+            <View style={styles.takePhotoOut}>
+              <View style={styles.takePhotoInnernewPhoto}>
+                <Image
+                  style={[styles.photoIcons, photoCard && styles.newPhoto]}
+                  source={
+                    photoCard
+                      ? { uri: photoCard }
+                      : require("../assets/images/notPhoto.png")
+                  }
+                />
+              </View>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+      {/* <Image
           style={[styles.photoIcons, photoCard && styles.newPhoto]}
           source={
             photoCard
               ? { uri: photoCard }
               : require("../assets/images/notPhoto.png")
           }
-        />
-      </TouchableOpacity>
+        /> */}
+      {/* </TouchableOpacity> */}
       {/* </View> */}
-      <Text
-        style={[styles.placeholderPhoto, photoCard && styles.placeholderHiden]}>
-        Завантажте фото
-      </Text>
+      <TouchableOpacity onPress={handleSelectPhoto}>
+        <Text
+          style={[
+            styles.placeholderPhoto,
+            photoCard && styles.placeholderHiden,
+          ]}>
+          Завантажте фото
+        </Text>
+      </TouchableOpacity>
       <TextInput
         placeholder="Назва..."
         style={[styles.textInput, isFocusedName && styles.textInputFocused]}
@@ -128,12 +196,12 @@ export const CreatePostsScreen = ({ navigation }) => {
         </TouchableOpacity>
       </View>
       <View style={styles.btnContainer}>
-        <TouchableOpacity style={styles.btn} onPress={() => null}>
+        {/* <TouchableOpacity style={styles.btn} onPress={() => null}>
           <Image
             style={styles.btnImage}
             source={require("../assets/images/trash.png")}
           />
-        </TouchableOpacity>
+        </TouchableOpacity> */}
       </View>
     </View>
   );
@@ -306,6 +374,39 @@ const styles = StyleSheet.create({
   containerMapFocused: {
     borderColor: "#FF6C00",
     borderWidth: 1,
+  },
+  camera: { flex: 1 },
+  photoView: {
+    flex: 1,
+    backgroundColor: "transparent",
+    justifyContent: "flex-end",
+  },
+
+  flipContainer: {
+    flex: 0.1,
+    alignSelf: "flex-end",
+  },
+
+  button: { alignSelf: "center" },
+
+  takePhotoOut: {
+    borderWidth: 2,
+    borderColor: "white",
+    height: 50,
+    width: 50,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 50,
+  },
+
+  takePhotoInner: {
+    borderWidth: 2,
+    borderColor: "white",
+    height: 40,
+    width: 40,
+    backgroundColor: "white",
+    borderRadius: 50,
   },
 });
 
