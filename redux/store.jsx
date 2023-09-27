a6c4903e7508dca36363337e452e0b39274bfbce
Dagaz <wlada801@gmail.com>
2023-09-27 08:56:23 +0300

    fix

--- redux/store.jsx
+++ redux/store.jsx
@@ -10,8 +10,8 @@ import {
   REGISTER,
 } from "redux-persist";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import rootReducer from "./rootReducer";
-
+// import rootReducer from "./rootReducer";
+//
 const persistConfig = {
   key: "root",
   storage: AsyncStorage,
